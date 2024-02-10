f9f5afeaf349949d738c8ecfb573cf5a2cd5325a
karim rahimian <karim.rahimian@gmail.com>
2024-02-10 09:24:48 +0330

    create models

--- cbioportal/settings.py
+++ cbioportal/settings.py
@@ -79,11 +79,22 @@ WSGI_APPLICATION = 'cbioportal.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cbioportal',
+        'USER':"root",
+        'PASSWORD':"123456",
+        'HOST': 'localhost',
+        "PORT":3307
+
     }
 }
+
+
 
 
 # Password validation
