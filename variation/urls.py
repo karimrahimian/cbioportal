bc95c9d1d1251df42e1a4718a7900b019ab36610
karim rahimian <karim.rahimian@gmail.com>
2024-02-13 09:41:40 +0330

    add analysis page

--- variation/urls.py
+++ variation/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('about',views.about,name='about'),
     path('login',views.login,name='login'),
     path('register',views.login,name='register'),
-    path('generate_data',views.generate_data,name='generate_data')
+    path('generate_data',views.generate_data,name='generate_data'),
+    path('analysis',views.analysis,name='analysis')
 ]
--- variation/views.py
+++ variation/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render
 from .generate_fake_data.faker_bio import FakerCbiooprtal
@@ -7,8 +8,8 @@ from .models import StudyType, Study
 # Create your views here.
 def index(request):
     context = {"hasError":False,"study_types":None}
-    context["study_types"]= StudyType.objects.all()
-
+    context["study_types"]= StudyType.objects.annotate(study_count=Count('study')).all()
+    context["studies"] = Study.objects.select_related('study_type').annotate(sample_count=Count('studysample')).all()
     return render(request, 'index.html',context = context)
 def about(request):
     return render(request, 'about.html')
@@ -18,6 +19,12 @@ def register(request):
     return render(request,'register.html')
 def generate_data(request):
     fake_generator = FakerCbiooprtal()
-    #fake_generator.generate_fake_patient(count=30)
-    fake_generator.generate_gene(30)
+    #fake_generator.generate_fake_patient(count=100)
+    #fake_generator.generate_gene(100)
+    #fake_generator.generate_sample(1000)
+    #fake_generator.generated_mutant_genes(800)
+    fake_generator.generate_study_sample(400)
     return HttpResponse("All data generated")
+
+def analysis(request):
+   return render(request,'analysis.html')
